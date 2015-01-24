d2cd1aa11d84a1791854b1c7101bb9c331cbd277
ndclinite <ndclinite@valdosta.edu>
2015-01-24 01:13:40 -0500

    added code to cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following two functions will utilize
+## stored functions within lists and lexographical
+## scoping to create a "pseudo-matrix" which is
+## is capable of storing its inverse.
 
-## Write a short comment describing this function
+## This function makes a list that contains
+## a matrix (x) and its inverse (y)
+## (y) is only generated once setinverse
+## is called.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	m <- NULL
+	set <- function(y)
+	{	x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(solve) m <<- solve
+	getinverse <- function() m
+	list(set = set, get = get,
+		setinverse = setinverse,
+		getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This is the thingy that checks if the matrix is inverted
+## If it is, it returns its stored inversion
+## If it is not, it tells the matrix to invert itself.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	## Return a matrix that is the inverse of 'x'
+	m <- x$getinverse()
+	if(!is.null(m))
+	{	message("getting cached data")
+		return(m)
+	}
+	data <- x$get()
+	m <- solve(data, ...)
+	x$setinverse(m)
+	m
 }
